2f570e611abae87fc04e11424209db2e744e4e5f
Sam Green <samueltylergreen@gmail.com>
2024-01-10 12:58:32 -0800

    Initial Commit

---
+++ QuestDeck/QuestDeckApp.swift
@@ -0,0 +1,17 @@
+//
+//  QuestDeckApp.swift
+//  QuestDeck
+//
+//  Created by Sam Green on 1/10/24.
+//
+
+import SwiftUI
+
+@main
+struct QuestDeckApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
